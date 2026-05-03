85ab1bf0713e3471aa74a28807e9436245a50485
eyal29 <eyalachheb2@gmail.com>
2026-05-03 17:35:18 +0200

    Remove key requirement for Cloud Run

--- src/__init__.py
+++ src/__init__.py
@@ -8,7 +8,6 @@ for env_var in [
     "ENV",
     "MLFLOW_SERVER",
     "MLFLOW_REGISTRY_NAME",
-    "GOOGLE_APPLICATION_CREDENTIALS",
 ]:
     if not os.getenv(env_var):
         raise Exception(f"Environment variable {env_var} must be defined.")
